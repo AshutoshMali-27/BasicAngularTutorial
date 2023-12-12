9131c61773edb900fec2ca8dccd2da3342691b00
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-12-12 23:50:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angulartutorial';
+  title = 'Helloworld my name is Ashutosh mali';
+  color="palevioletred";
+users=['Ashutosh','naksh','omkar','rohit','rahul'];
+
+
+userDetailofsocial=[
+  {name:'ashutosh',email:'ashutosh@123.com',phone:'8051' ,socialAccounts:['facebook','insta','gmail']},
+  {name:'naksh',email:'naksh@123.com',phone:'8052' ,socialAccounts:['facebook','insta','gmail']},
+  {name:'Omkar',email:'Omkar@123.com',phone:'8053',socialAccounts:['facebook','insta','gmail']},
+  {name:'Rohit',email:'Rohit@123.com',phone:'8054',socialAccounts:['facebook','insta','gmail']},
+];
+
+userDetail=[
+  {name:'ashutosh',email:'ashutosh@123.com',phone:'8051'},
+  {name:'naksh',email:'naksh@123.com',phone:'8052'},
+  {name:'Omkar',email:'Omkar@123.com',phone:'8053'},
+  {name:'Rohit',email:'Rohit@123.com',phone:'8054'},
+];
+  data="hello world";
+  num =100;
+  name='peter';
+  disable=false;
+  show='yes';
+  user={
+    name:'ashutosh',
+    age:24
+
+  }
+  today=Date();
+  getName(name:string){
+    alert(name);
+  }
+
+  getnumber(){
+    alert("this is a number");
+  }
+
+  getdate(){
+    console.log("hello");
+  }
+
+  getevent(val:string){
+    console.warn(val)
+  }
+count=0
+  counter(type:string){
+    type ==='add'?this.count++:this.count--
+  }
+
+  updateColor(){
+    this.color='brown';
+  }
+
+  bgcolor='red';
+  updatebgcolor(){
+    this.bgcolor='blue';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Comp1Component } from './Component/comp1/comp1.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { UserpipePipe } from './userpipe.pipe';
+import { HeaderComponent } from './Component/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    UserpipePipe,
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
